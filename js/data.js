18bf91a5f78c38d0fb1871b060eeea84d9675561
Jacopo <jacopobraccio@yahoo.it>
2021-04-24 17:07:45 +0200

    Update for translations

--- js/data.js
+++ js/data.js
@@ -1,46 +1,75 @@
 // Procrastination methods
-const methods = [
+const suggestions = [
     {
-        "description":"Stare at the wall for 10 minutes",
+        "description":{
+            "eng": "Stare at the wall for 10 minutes",
+            "it": "Fissa il muro per 10 minuti"
+        
+        }
     },
     {
-        "description":"Look at your ex on Instagram",
+        "description":{
+            "eng": "Look at your ex on Instagram",
+            "it": "Vai sul profilo instagram della tua ex"
+        
+        }
     },
     {
-        "description":"Watch some youtube videos",
+        "description":{
+            "eng": "Watch some youtube videos",
+            "it": "Guarda un video su youtube"
+        }
     },
     {
-        "description":"Join some boomer facebook group",
+        "description": {
+            "eng":"Join some boomer facebook group",
+            "it":"Entra su un gruppo boomer di facebook"
+        }
     },
     {
-        "description":"Open TikTok",
+        "description": {
+            "eng":"Open TikTok",
+            "it":"Apri TikTok"
+        }
     },
     {
-        "description":"Spin the wheel again",
+        "description": {
+            "eng":"Push the button again",
+            "it":"Premi il bottone di nuovo"
+        }
     },
     {
-        "description":"Listen to some Korean Pop music",
+        "description": {
+            "eng":"Listen to some Korean Pop music",
+            "it":"Ascolta del K-Pop"
+        }
     },
     {
-        "description":"Learn new pen tricks",
+        "description": {
+            "eng":"Eat something",
+            "it": "Mangia qualcosa"
+        }
     },
     {
-        "description":"Eat something",
+        "description": {
+            "eng":"Call you bf",
+            "it": "Chiama il tuo/la tua bf"
+        }  
     },
     {
-        "description":"Call you bf",  
+        "description": {
+            "eng": "Write a poem", 
+            "it": "Scrivi una poesia"
+        } 
     },
     {
-        "description":"Write a poem",  
+        "description": {
+            "eng": "Write a song",
+            "it": "Inventa una canzone"
+        }  
     },
     {
-        "description":"Invent a song",  
-    },
-    {
-        "description":"Your dog is cute, pet it!",  
-    },
-    {
-        "description":"Instagram is full of meme, go watch them!",  
-    }
-
+        "description":"Instagram is full of meme, go watch them!",
+        "it":"Instagram è pieno di meme, vedine qualcuno"
+    }  
 ]
